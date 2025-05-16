0e94de8276002e71b64ddc2188d161d2154e54cc
Juts2 <855924+Juts2@users.noreply.github.com>
2025-05-16 03:31:18 -0400

    updated window size

--- gameconfig.py
+++ gameconfig.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(600, 600)
     widget.show()
 
     sys.exit(app.exec())
